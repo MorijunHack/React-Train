39a53e2acd91dd7500fb295159331f5fa95a7847
morijun <morijun0806@gmail.com>
2019-08-05 00:42:43 +0900

    React Component

--- 3-Project/react_app/src/App.js
+++ 3-Project/react_app/src/App.js
@@ -1,9 +1,10 @@
-// react.jsに記述されているReact（exportされてるもの）とComponentをインポートしている。後者はexportされていないので、{}で囲って表現する
+// react.jsに記述されているReact（exportされてるもの）とComponentをインポートしている。
+// 後者はexportされていないので、{}で囲って表現する
 import React, { Component } from 'react';
 import './App.css';
 
 // 元々は関数コンポーネント
-// function App() {
+//； function App() {
 //   return (
 //     <div>
 //       <h1>React</h1>
@@ -14,13 +15,19 @@ import './App.css';
 // }
 
 // 教材ではクラスに変えたいようなので、こちらに書き換えた
+// 関数コンポーネントよりクラスの方が便利とのこと。これをデフォルトとしたい
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.title = props.title;
+    this.message = props.message;
+  }
+
   render() {
     return (
       <div>
-        <h1>React</h1>
-        <p>This is sample component.</p>
-        <p>これはコンポーネントのサンプルですよん。</p>
+        <h1>{this.title}</h1>
+        <p>{this.message}</p>
       </div>
     );
   }
