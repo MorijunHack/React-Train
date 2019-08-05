80c58301613f6e74c67cd37246caed7286b7eb3d
morijun <morijun0806@gmail.com>
2019-08-06 00:39:43 +0900

    change display by event

--- 3-Project/react_app/src/App.js
+++ 3-Project/react_app/src/App.js
@@ -4,30 +4,58 @@ import React, { Component } from 'react';
 import './App.css';
 import Rect from './Rect';
 
-// 元々は関数コンポーネント
-//； function App() {
-//   return (
-//     <div>
-//       <h1>React</h1>
-//       <p>This is sample component.</p>
-//       <p>これはサンプルのコンポーネントですよん</p>
-//     </div>
-//   );
-// }
-
-// 教材ではクラスに変えたいようなので、こちらに書き換えた
-// 関数コンポーネントよりクラスの方が便利とのこと。これをデフォルトとしたい
 class App extends Component {
+  msgStyle = {
+    fontSize: "24pt",
+    color: "#900",
+    margin: "20px 0",
+    padding: "5px",
+    borderBottom: "2px solid #900"
+  };
+
+  msgStyle2 = {
+    fontSize: "20pt",
+    color: "white",
+    background: "#900",
+    margin: "20px 0",
+    padding: "5px",
+    borderBottom: "2px solid #900"
+  };
+
+  btnStyle = {
+    fontSize: "16pt",
+    padding: "10px"
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      flg: true,
+      msg: 'Count start!',
+      counter: 0
+    };
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction() {
+    this.setState((state)=>({
+      counter: state.counter + 1,
+      msg: state.counter,
+      flg: !state.flg,
+    }));
+  }
 
   render() {
-    return (
-      <div>
-        <h1>React</h1>
-        <Rect x="50" y="50" w="150" h="150" c="#6ff9" r="50" />
-        <Rect x="159" y="100" w="150" h="150" c="#f6f9" r="75" />
-        <Rect x="100" y="150" w="150" h="150" c="#6669" r="25" />
-      </div>
-    );
+    return <div>
+      <h1>React</h1>
+      {
+        this.state.flg ?
+        <p style={this.msgStyle}>{this.state.msg}</p>
+        :
+        <p style={this.msgStyle2}>{this.state.msg}</p>
+      }
+      <button style={this.btnStyle} onClick={this.doAction}>Click</button>
+    </div>;
   }
 }
 
