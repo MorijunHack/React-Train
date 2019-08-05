f7afa0bf803adef7d94d6915d109deb59d2125f0
morijun <morijun0806@gmail.com>
2019-08-05 21:42:50 +0900

    only display

--- 3-Project/react_app/src/App.js
+++ 3-Project/react_app/src/App.js
@@ -2,6 +2,7 @@
 // 後者はexportされていないので、{}で囲って表現する
 import React, { Component } from 'react';
 import './App.css';
+import Rect from './Rect';
 
 // 元々は関数コンポーネント
 //； function App() {
@@ -17,17 +18,14 @@ import './App.css';
 // 教材ではクラスに変えたいようなので、こちらに書き換えた
 // 関数コンポーネントよりクラスの方が便利とのこと。これをデフォルトとしたい
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.title = props.title;
-    this.message = props.message;
-  }
 
   render() {
     return (
       <div>
-        <h1>{this.title}</h1>
-        <p>{this.message}</p>
+        <h1>React</h1>
+        <Rect x="50" y="50" w="150" h="150" c="#6ff9" r="50" />
+        <Rect x="159" y="100" w="150" h="150" c="#f6f9" r="75" />
+        <Rect x="100" y="150" w="150" h="150" c="#6669" r="25" />
       </div>
     );
   }
